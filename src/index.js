86dfeda48c39dc1b00ac95af44b63d62d9fb2d45
fjcamillo <fjcamillo.dev@gmail.com>
2017-07-29 20:42:49 +0800

    index.js broken again, rerun firmata ang gort installation still not working

--- src/index.js
+++ src/index.js
@@ -1,20 +1,26 @@
 'use strict'
 
-const cylon = require('cylon')
+let cylon = require('cylon')
 
 cylon.robot({
-  connections: {
-    arduino: { adaptor: 'firmata', port: '/dev/ttyACM0' }
+  'connections': {
+    'arduino': { 'adaptor': 'firmata', 'port': '/dev/ttyACM0' }
   },
 
-  devices: {
-    led: { driver: 'led', pin: 13}
+  'devices': {
+    'led': { 'driver': 'led', 'pin': 12}
   },
 
   work: (my) => {
-    every((.5).second(), () => {
-      my.led.toggle()
-      console.log('1')
+    every((1).second(), () => {
+      my.led.turnOn((err, val) => {
+          if(err){
+            console.log(err)
+          }else{
+            console.log(val)
+          }
+      })
+      // console.log('1')
     })
   }
 }).start()
