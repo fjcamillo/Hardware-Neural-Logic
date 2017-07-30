ff02e394bc1b6f45cf9632ee2ef93f8af283e9e6
fjcamillo <fjcamillo.dev@gmail.com>
2017-07-30 20:34:42 +0800

    facing error with arduino

--- src/index.js
+++ src/index.js
@@ -1,26 +1,15 @@
-'use strict'
+var Cylon = require('cylon');
 
-let cylon = require('cylon')
-
-cylon.robot({
-  'connections': {
-    'arduino': { 'adaptor': 'firmata', 'port': '/dev/ttyACM0' }
+Cylon.robot({
+  connections: {
+    arduino: { adaptor: 'firmata', port: '/dev/ttyACM0' }
   },
 
-  'devices': {
-    'led': { 'driver': 'led', 'pin': 12}
+  devices: {
+    led: { driver: 'led', pin: 13 }
   },
 
-  work: (my) => {
-    every((1).second(), () => {
-      my.led.turnOn((err, val) => {
-          if(err){
-            console.log(err)
-          }else{
-            console.log(val)
-          }
-      })
-      // console.log('1')
-    })
+  work: function(my) {
+    every((1).second(), my.led.toggle);
   }
-}).start()
+}).start();
