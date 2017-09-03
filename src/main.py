023d8c8c6f475c20827439b520b554c53c26c1e5
fjcamillo <fjcamillo.dev@gmail.com>
2017-09-03 20:22:33 +0800

    added logic type inputs and feature inputs

--- src/main.py
+++ src/main.py
@@ -6,5 +6,24 @@ arduino = serial.Serial('/dev/ttyACM0', 9600, timeout=.1 )
 with open('./data.json', 'r') as f:
     data = json.load(f)
 
-while true:
-    arduino.write('1')
+def getweights(data, logic):
+    return data[logic]['features'], data[logic]['labels'], data[logic]['weights']
+
+
+inp = input("""
+Logic Type:
+1. AND
+2. OR
+3. NOT
+"""
+)
+
+print(f"Chosen Logic Type {inp}")
+
+feature = input("""
+Input Type:
+1. [0, 0]
+2. [0, 1]
+3. [1, 0]
+4. [1, 1]
+""")
