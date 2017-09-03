ba6adb847d6f86eac57b8a58b3ca0a7a4a0f31f2
fjcamillo <fjcamillo.dev@gmail.com>
2017-09-03 12:19:24 +0800

    migrated to jonny five, cylon js is having a problem with linking the serialport library

--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
-import Cylon from 'cylon'
-import linearmodel from './model'
-import cost from './costfunction'
-import data from './data.json'
+// import Cylon from 'cylon'
+// import SerialPort from 'serialport'
+import five from 'johnny-five'
+// import linearmodel from './model'
+// import cost from './costfunction'
+// import data from './data.json'
+
+// const port = new SerialPort('/dev/ttyACM0', {
+//   baudRate: 9600
+// })
+
+
 // Cylon.robot({
 //   connections: {
-//     arduino: { adaptor: 'firmata', port: '/dev/ttyACM0' }
+//     arduino: { adaptor: 'firmata', port: port }
 //   },
 //
 //   devices: {
@@ -15,3 +23,10 @@ import data from './data.json'
 //     every((1).second(), my.led.toggle);
 //   }
 // }).start();
+
+const board  = new five.Board()
+
+board.on('ready', () => {
+  const led = new five.Led(13)
+  led.blink
+})
